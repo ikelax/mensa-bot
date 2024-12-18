239c4ad1d691c098826302cf14de3798c4e30ed5
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2024-12-18 08:54:50 +0100

    Implement proof of concept
    Currently, the bot replys to every message with the mealplan for today.
    In private chats, you can choose the mealplan for today.
    
    Also, Prettier was configured for a consistent formatting.

---
+++ main.js
@@ -0,0 +1,66 @@
+import ky from "ky";
+import { Bot, InlineQueryResultBuilder } from "grammy";
+
+function isToday(dateString) {
+  var inputDate = new Date(dateString);
+  var todaysDate = new Date();
+  return inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0);
+}
+
+function test(mealplans) {
+  const todaysMealplan = mealplans.days.find((day) => isToday(day.date));
+
+  if (todaysMealplan == undefined) {
+    return "Heute gibt es kein Essen in der Mensa!";
+  }
+
+  const counters = todaysMealplan.counters.filter((meal) => meal.id !== "info");
+
+  return counters
+    .map((counter) => {
+      const counterTitle = `__*${counter.displayName} - ${counter.description}*__\n`;
+      const meals = counter.meals;
+      const mealsString = meals
+        .map((meal) => {
+          const price = meal?.prices?.s ?? "???"
+          const mealTitle = `${meal.name} für ${price}€`;
+
+          if (meal.name === "Salatbuffet") {
+            return mealTitle;
+          }
+
+          const extras = meal.components
+            .map((component) => component.name)
+            .join("\n");
+          return mealTitle + "\n" + extras;
+        })
+        .join("\n\n");
+
+      return counterTitle + mealsString;
+    })
+    .join("\n\n");
+}
+
+async function getMealplans() {
+  const url = "https://mensaar.de/api/2/TFtD8CTykAXXwrW4WBU4/1/de/getMenu/sb";
+  const mealplans = await ky.get(url).json();
+  const escapedMealplans = test(mealplans).replaceAll('-', '\\-').replaceAll('(', '\\(').replaceAll(')', '\\)');
+  const link = "[Speiseplan](https://mensaar.de/#/menu/sb)"
+  return escapedMealplans + "\n" + link
+}
+
+const bot = new Bot("5191974791:AAGz7Tns69X3L4ZN1g8ptrLQaNuYWUFdu64");
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.on("message", async (ctx) =>
+  ctx.reply(await getMealplans(), { parse_mode: "MarkdownV2" }),
+);
+bot.on("inline_query", async (ctx) => {
+  const result = InlineQueryResultBuilder.article("id", "Mensa UdS").text(
+    await getMealplans(),
+    { parse_mode: "MarkdownV2" },
+  );
+  await ctx.answerInlineQuery([result], { cache_time: 0 });
+});
+
+bot.start();
