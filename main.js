029a3b0151c8be75aaa1ef4d14162d8fa411fd2e
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2024-12-19 17:49:59 +0100

    Prepare code for deployment on Deno Deploy
    I followed the guide [0] for Deno Deploy. Since Deno has an integrated
    formatter, I removed Prettier. Because the bot hosted has to use
    Webhooks, I added a dev version which uses Long Polling as it does not
    require a web server.
    
    [0]: https://grammy.dev/hosting/deno-deploy

--- main.js
+++ main.js
@@ -1,66 +1,18 @@
-import ky from "ky";
-import { Bot, InlineQueryResultBuilder } from "grammy";
+import { webhookCallback } from "grammy";
+import { bot } from "./bot.js";
 
-function isToday(dateString) {
-  var inputDate = new Date(dateString);
-  var todaysDate = new Date();
-  return inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0);
-}
+const handleUpdate = webhookCallback(bot, "std/http");
 
-function test(mealplans) {
-  const todaysMealplan = mealplans.days.find((day) => isToday(day.date));
-
-  if (todaysMealplan == undefined) {
-    return "Heute gibt es kein Essen in der Mensa!";
+Deno.serve(async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
   }
-
-  const counters = todaysMealplan.counters.filter((meal) => meal.id !== "info");
-
-  return counters
-    .map((counter) => {
-      const counterTitle = `__*${counter.displayName} - ${counter.description}*__\n`;
-      const meals = counter.meals;
-      const mealsString = meals
-        .map((meal) => {
-          const price = meal?.prices?.s ?? "???"
-          const mealTitle = `${meal.name} für ${price}€`;
-
-          if (meal.name === "Salatbuffet") {
-            return mealTitle;
-          }
-
-          const extras = meal.components
-            .map((component) => component.name)
-            .join("\n");
-          return mealTitle + "\n" + extras;
-        })
-        .join("\n\n");
-
-      return counterTitle + mealsString;
-    })
-    .join("\n\n");
-}
-
-async function getMealplans() {
-  const url = "https://mensaar.de/api/2/TFtD8CTykAXXwrW4WBU4/1/de/getMenu/sb";
-  const mealplans = await ky.get(url).json();
-  const escapedMealplans = test(mealplans).replaceAll('-', '\\-').replaceAll('(', '\\(').replaceAll(')', '\\)');
-  const link = "[Speiseplan](https://mensaar.de/#/menu/sb)"
-  return escapedMealplans + "\n" + link
-}
-
-const bot = new Bot("5191974791:AAGz7Tns69X3L4ZN1g8ptrLQaNuYWUFdu64");
-
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.on("message", async (ctx) =>
-  ctx.reply(await getMealplans(), { parse_mode: "MarkdownV2" }),
-);
-bot.on("inline_query", async (ctx) => {
-  const result = InlineQueryResultBuilder.article("id", "Mensa UdS").text(
-    await getMealplans(),
-    { parse_mode: "MarkdownV2" },
-  );
-  await ctx.answerInlineQuery([result], { cache_time: 0 });
+  return new Response();
 });
-
-bot.start();
