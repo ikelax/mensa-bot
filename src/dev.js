0001b2aeb672c71917def7a40b60d185780b7939
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-05-28 00:43:30 +0200

    Set time zone replacing Date.now with TZDate.tz
    The package @date-fns/tz was installed. It provides the static method
    TZDate.tz which initialises a Date object in the passed time zone.
    
    The command /time was added for testing the function getTime().
    
    See also: https://github.com/date-fns/tz
    Related to: https://github.com/ikelax/mensa-bot/issues/43

--- src/dev.js
+++ src/dev.js
@@ -2,7 +2,6 @@ import { getMensaBot } from "./bot.js";
 import process from "node:process";
 
 if (import.meta.main) {
-  process.env.TZ = "Europe/Berlin";
   const bot = getMensaBot(Deno.env.get("TELEGRAM_BOT_DEV_TOKEN"));
   bot.start();
 }
